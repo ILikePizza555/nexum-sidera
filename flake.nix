a92dc6bfcc5733374f8ad535b0a59cd3d4b82084
Izzy Lancaster <izzylan@Izzy-Luna.local>
2023-01-21 15:45:16 -0800

    New flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    };
+
+    outputs = {self, nixpkgs, flake-parts}: let
+        forEachSystem = systems: callback: lib.genAttrs systems (system:
+            let pkgs = nixpkgs.legacyPackages.${system}; in
+            callback system pkgs
+        );
+
+        defaultSystems = ["aarch64-darwin"];
+    in
+    {
+        devShells = forEachSystem defaultSystems (system: pkgs: {
+            default = pkgs.mkShell {
+                package = with pkgs; [
+                    nodejs
+                    nodePackages.yarn
+                ];
+            };
+        });
+    };
+}
