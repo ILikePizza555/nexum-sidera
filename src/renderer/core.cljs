2450fbe054f384e4930057f9836b1ea1be684866
Izzy Lancaster <izzylan@Izzy-Luna.local>
2023-01-30 22:47:47 -0800

    Rudimentary layout code

--- src/renderer/core.cljs
+++ src/renderer/core.cljs
@@ -8,23 +8,27 @@
 (defonce root (createRoot (js/document.getElementById "app-container")))
 (defonce state (atom 0))
 
+(defn tree-view-item [{:keys [label]} & children]
+  [:li.tree-view-item 
+   [:div 
+    [:span.icon-container (if children "\u25B9" ())]
+    [:span.label label]]
+   (if children [:ul.tree-view-group children] ())])
+
 (defn root-component []
-  [:ul.tree-view.tree-view-group.tree-view-root
-   [:li.tree-view.tree-view-item "Leaf Component"]
-   [:li.tree-view.tree-view-group "Subtree"
-    [:ul
-     [:li "Leaf 1"]
-     [:li "Leaf 2"]
-     [:li "Leaf 3"]]]
-   [:li.tree-view "Subtree with sub-subtree"
-    [:ul
-     [:li "Leaf 1"]
-     [:li "Sub-subtree"
-      [:ul
-       [:li "Leaf 2"]
-       [:li "Leaf 3"]
-       [:li "Leaf 4"]]]
-     [:li "Leaf 5"]]]])
+  [:ul.tree-view-group.tree-view-root
+   [tree-view-item {:label "Leaf Component"}]
+   [tree-view-item {:label "Subtree"}
+    [tree-view-item {:label "Leaf 1"}]
+    [tree-view-item {:label "Leaf 2"}]
+    [tree-view-item {:label "Leaf 3"}]]
+   [tree-view-item {:label "Subtree with sub-subtree"}
+    [tree-view-item {:label "Leaf 1"}]
+    [tree-view-item {:label "Sub-subtree"}
+     [tree-view-item {:label "Leaf 2"}]
+     [tree-view-item {:label "Leaf 3"}]
+     [tree-view-item {:label "Leaf 4"}]]
+    [tree-view-item {:label "Leaf 5"}]]])
 
 (defn ^:dev/after-load start! []
   (.render root (as-element [root-component])))
