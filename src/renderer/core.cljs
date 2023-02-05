174185a4190730fbcdae465212b8dc4cf8347587
Izzy Lancaster <izzylan@Izzy-Luna.local>
2023-02-05 14:49:03 -0800

    Added old scaffolding code back

--- src/renderer/core.cljs
+++ src/renderer/core.cljs
@@ -1,10 +1,31 @@
 (ns renderer.core
   (:require [helix.core :refer [defnc $]]
+            [helix.dom :as d]
             ["react-dom/client" :as rdom]))
 
 (enable-console-print!)
 
-(defnc app )
+(defnc tree-view-item [{:keys [label children]}]
+  (d/li
+   (d/div
+    (d/span "Icon Container")
+    (d/span label))
+   (when children (d/ul children))))
+
+(defnc app []
+  (d/ul
+   ($ tree-view-item {:label "🍃 Leaf Component"})
+   ($ tree-view-item {:label "🌲 Subtree"}
+      ($ tree-view-item {:label "Leaf 1"})
+      ($ tree-view-item {:label "Leaf 2"})
+      ($ tree-view-item {:label "Leaf 3"}))
+   ($ tree-view-item {:label "🎋 Subtree with sub-subtree"}
+      ($ tree-view-item {:label "Leaf 1"})
+      ($ tree-view-item {:label "Sub-subtree"}
+         ($ tree-view-item {:label "Leaf 2"})
+         ($ tree-view-item {:label "Leaf 3"})
+         ($ tree-view-item {:label "Leaf 3"}))
+      ($ tree-view-item {:label "Leaf 5"}))))
 
 (defonce root (rdom/createRoot (js/document.getElementById "app-container")))
 (defn ^:dev/after-load start! []
