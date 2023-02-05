7467f088f5467bd6cbd397c0a5be0d910a30405f
Izzy Lancaster <izzylan@Izzy-Luna.local>
2023-02-05 14:21:51 -0800

    Switched to using helix

--- src/renderer/core.cljs
+++ src/renderer/core.cljs
@@ -1,42 +1,11 @@
 (ns renderer.core
-  (:require [reagent.core :refer [atom as-element]]
-            [reagent.dom :as rd]
-            ["react-dom/client" :refer [createRoot]]))
+  (:require [helix.core :refer [defnc $]]
+            ["react-dom/client" :as rdom]))
 
 (enable-console-print!)
 
-(defonce root (createRoot (js/document.getElementById "app-container")))
+(defnc app )
 
-(defn collapse [is-collapsed & children]
-  (js/console.log "Collapse render " is-collapsed)
-  (into
-   [:div.collapse {:class (when is-collapsed "active")}] children))
-
-(defn tree-view-item [{:keys [label]} & children]
-  (let [is-collapsed (atom false)]
-    (fn [{:keys [label]} & children]
-      [:li.tree-view-item 
-       [:div
-        {:on-click #(swap! is-collapsed not)}
-        [:span.icon-container (when children "\u25B9")]
-        [:span.label label]]
-       (when children [collapse @is-collapsed
-                       (into [:ul.tree-view-group] children)])])))
-
-(defn root-component []
-  [:ul.tree-view-group.tree-view-root
-   [tree-view-item {:label "Leaf Component"}]
-   [tree-view-item {:label "Subtree"}
-    [tree-view-item {:label "Leaf 1"}]
-    [tree-view-item {:label "Leaf 2"}]
-    [tree-view-item {:label "Leaf 3"}]]
-   [tree-view-item {:label "Subtree with sub-subtree"}
-    [tree-view-item {:label "Leaf 1"}]
-    [tree-view-item {:label "Sub-subtree"}
-     [tree-view-item {:label "Leaf 2"}]
-     [tree-view-item {:label "Leaf 3"}]
-     [tree-view-item {:label "Leaf 4"}]]
-    [tree-view-item {:label "Leaf 5"}]]])
-
+(defonce root (rdom/createRoot (js/document.getElementById "app-container")))
 (defn ^:dev/after-load start! []
-  (.render root (as-element [root-component])))
+  (.render root ($ app)))
