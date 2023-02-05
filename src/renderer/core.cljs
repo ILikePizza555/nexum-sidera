6af83eaf7da6ce9cbe93e118b49f6a3077dacf6a
Izzy Lancaster <izzylan@Izzy-Luna.local>
2023-02-05 15:15:45 -0800

    Use rendered styles

--- src/renderer/core.cljs
+++ src/renderer/core.cljs
@@ -1,31 +1,42 @@
 (ns renderer.core
   (:require [helix.core :refer [defnc $]]
             [helix.dom :as d]
-            ["react-dom/client" :as rdom]))
+            ["react-dom/client" :as rdom]
+            [dv.cljs-emotion :refer [defstyled]]))
 
 (enable-console-print!)
 
-(defnc tree-view-item [{:keys [label children]}]
+(defstyled tree-root :ul
+  {:list-style-type "none"
+   :margin 0
+   :padding 0})
+
+(defstyled tree-content :ul
+  {:list-style-type "none"
+   :margin 0
+   :padding-inline-start "0.7em"})
+
+(defnc tree [{:keys [label children]}]
   (d/li
    (d/div
     (d/span "Icon Container")
     (d/span label))
-   (when children (d/ul children))))
+   (when children (tree-content children))))
 
 (defnc app []
-  (d/ul
-   ($ tree-view-item {:label "🍃 Leaf Component"})
-   ($ tree-view-item {:label "🌲 Subtree"}
-      ($ tree-view-item {:label "Leaf 1"})
-      ($ tree-view-item {:label "Leaf 2"})
-      ($ tree-view-item {:label "Leaf 3"}))
-   ($ tree-view-item {:label "🎋 Subtree with sub-subtree"}
-      ($ tree-view-item {:label "Leaf 1"})
-      ($ tree-view-item {:label "Sub-subtree"}
-         ($ tree-view-item {:label "Leaf 2"})
-         ($ tree-view-item {:label "Leaf 3"})
-         ($ tree-view-item {:label "Leaf 3"}))
-      ($ tree-view-item {:label "Leaf 5"}))))
+  (tree-root
+   ($ tree {:label "🍃 Leaf Component"})
+   ($ tree {:label "🌲 Subtree"}
+      ($ tree {:label "Leaf 1"})
+      ($ tree {:label "Leaf 2"})
+      ($ tree {:label "Leaf 3"}))
+   ($ tree {:label "🎋 Subtree with sub-subtree"}
+      ($ tree {:label "Leaf 1"})
+      ($ tree {:label "Sub-subtree"}
+         ($ tree {:label "Leaf 2"})
+         ($ tree {:label "Leaf 3"})
+         ($ tree {:label "Leaf 3"}))
+      ($ tree {:label "Leaf 5"}))))
 
 (defonce root (rdom/createRoot (js/document.getElementById "app-container")))
 (defn ^:dev/after-load start! []
