bdc96cbedad5b5115974660da74f9b4d0c0d85a5
Izzy Lancaster <avrisaac555@gmail.com>
2023-03-09 18:28:27 -0800

    Added some wrapper functions for sqlite3

--- src/main/db.cljs
+++ src/main/db.cljs
@@ -1,7 +1,7 @@
 (ns main.db
   (:require ["sqlite3" :as sqlite3]
-            [lambdaisland.glogi :as log]))
-
+            [lambdaisland.glogi :as log]
+            [cljs.core.async :refer [put!]]))
 
 (defn create-connection [path]
   (let [sqlite3 (.verbose sqlite3)
@@ -9,3 +9,35 @@
     (-> (new Database path)
         (.on "trace" #(log/trace :query %))
         (.on "profile" #(log/trace :query %1 :execution-time %2)))))
+
+(defn ^:private run-cb [out]
+  (fn [err] (if err
+              (put! out {:error err})
+              (this-as this
+                       (put! out {:last-id (.lastID this)
+                                  :changes (.changes this)})))))
+
+(defn run
+  "CLJS Wrapper around the [sqlite3/Database.run()](https://github.com/TryGhost/node-sqlite3/wiki/API#runsql--param---callback) method.
+   Calls `run()` on the provided function with the provided `sql` query string, cljs->js converted `params`, and a callback function which puts
+   the result of the query -- or any errors -- on the `out` channel."
+  ([db sql params] (run db sql params nil))
+  ([db sql params out]
+   (.run db sql (clj->js params)
+         (when out (run-cb out)))))
+
+(defn prepare
+  ([db sql] (prepare db sql [] nil))
+  ([db sql params] (prepare db sql params nil))
+  ([db sql params out]
+   (.prepare db sql (clj->js params)
+             (when out (fn [err] (if err
+                                   (put! out {:error err})
+                                   (put! out :success)))))))
+
+(defn statement-run
+  ([stmt] (statement-run stmt [] nil))
+  ([stmt params] (statement-run stmt params nil))
+  ([stmt params out]
+   (.run stmt (clj->js params)
+         (when out (run-cb out)))))
